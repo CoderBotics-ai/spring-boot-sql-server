f4153c64c40c8617496400d4b1df7d3299e37cea
Pushkar Patel <git@thepushkarp.com>
2024-06-13 03:04:19 +0530

    convert sqlalchemy to pymongo

--- src/main/java/com/bezkoder/spring/mssql/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/mssql/repository/TutorialRepository.java
@@ -1,12 +1,15 @@
-package com.bezkoder.spring.mssql.repository;
+
+package com.bezkoder.spring.mongodb.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-import com.bezkoder.spring.mssql.model.Tutorial;
+import com.bezkoder.spring.mongodb.model.Tutorial;
 
-public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+@Repository
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
   List<Tutorial> findByPublished(boolean published);
   List<Tutorial> findByTitleContaining(String title);
 }
